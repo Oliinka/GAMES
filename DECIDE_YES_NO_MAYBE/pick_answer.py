9029e7ff8eb404b104b736be2aaf0e779b1c99cb
Oliinka <olga.titzenthaler@gmail.com>
2023-12-27 12:19:27 +0100

    program picks random answer from the list

--- DECIDE_YES_NO_MAYBE/pick_answer.py
+++ DECIDE_YES_NO_MAYBE/pick_answer.py
@@ -5,9 +5,10 @@ class AnswerPicker:
     answer = ("YES", "NO", "MAY BE")
 
     def __init__(self):
-        return f"The possible choices are: {self.answer}"
-    def __int__(self):
         pass
+
+    def __str__(self):
+        return f"The possible choices are: {self.answer}"
 
     def decide (self):
         return random.choice(self.answer)
