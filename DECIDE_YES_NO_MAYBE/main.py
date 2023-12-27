129d5fcd702797e5e4a05c13a209a41d051b17f0
Oliinka <olga.titzenthaler@gmail.com>
2023-12-27 12:19:27 +0100

    program picks random answer from the list

--- DECIDE_YES_NO_MAYBE/main.py
+++ DECIDE_YES_NO_MAYBE/main.py
@@ -3,7 +3,7 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from pick_answer import AnswerPicker:
+from pick_answer import AnswerPicker
 
 print("""
 -------------------------------------
@@ -15,22 +15,26 @@ answer_picker = AnswerPicker()
 while True:
     print("""
     Choose one of the following:
-    1 - GET YOUR ANSWER
-    2 - end the program
+    1 - THINK OF THE QUESTION, PRESS "1" AND GET YOUR ANSWER
+    2 - NO MORE QUESTIONS? END THE PROGRAM BY PRESSING "2"  
+    3 - FIND OUT WHAT ARE THE POSSIBLE ANSWERS
     --------------------------------------
     """)
 
     choice_number = input("> ")
 
-#5 searches ensurer by its age
+    # 1 pick random word from the list "answer" in the class AnswerPicker in the python file pick_answer.py
     if choice_number == "1":
-        print(found_insurer)
+        print(answer_picker.decide())
 
-
-#9 exiting the program loop
-    elif choice_number == "9":
+    # 2 exiting the program loop
+    elif choice_number == "2":
         print("Exiting the program.")
         break  # Exit the loop and end the program
 
+    elif choice_number == "3":
+        print(answer_picker.__str__())
+
     else:
-        print("Wrong choice! For exit press 9 and enter.")
+        print("Wrong choice! For exit press 2 and enter.")
+
--- DECIDE_YES_NO_MAYBE/pick_answer.py
+++ DECIDE_YES_NO_MAYBE/pick_answer.py
@@ -2,12 +2,13 @@ import random
 
 
 class AnswerPicker:
-    answer = ("YES", "NO", "MAY BE")
+    answer = ("YES", "NO", "MAY BE", "YOU WILL FIND OUT LATER")
 
     def __init__(self):
-        return f"The possible choices are: {self.answer}"
-    def __int__(self):
         pass
+
+    def __str__(self):
+        return f"The possible choices are: {self.answer}"
 
     def decide (self):
         return random.choice(self.answer)
